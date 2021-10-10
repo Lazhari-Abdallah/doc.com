c0bdc79b95241e539adfa851f7d174ae057aac7f
Lazhari-Abdallah <lazhariabdallah@gmail.com git config --global user.email lazhariabdallah@gmail.com>
2021-10-10 01:29:31 +0100

    function

--- main.js
+++ main.js
@@ -1,4 +1,31 @@
-button.onclik =()
-const user ={
-    
+function submit() {
+    var name = document.getElementById("name").value;
+    var email = document.getElementById("email").value;
+    var adresse = document.getElementById("adresse").value;
+    var date = document.getElementById("date").value;
+    var city = document.getElementById("city").value;
+    var coment = document.getElementById("coment").value;
+    if (name && email &&adresse&&date&&city&&coment) {
+        localStorage.setItem("name", name);
+        localStorage.setItem("email", email);
+        localStorage.setItem("adresse", adresse);
+        localStorage.setItem("date", date);
+        localStorage.setItem("city", city);
+        localStorage.setItem("coment", coment);
+
+    } else {
+        alert("Missing information");
+    }
 }
+
+window.onload = function () {
+    var name = localStorage.getItem("name");
+    var coment = localStorage.getItem("coment");
+
+    console.log(name, coment);
+    if (!name || !coment) {
+        return;
+    }
+    document.getElementsByClassName("name")[0].innerText = name;
+    document.getElementsByClassName("coment")[0].innerText =coment;
+};
